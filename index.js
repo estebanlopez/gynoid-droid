b4a7b87d02684c0d6a7e281d55b8ce4fc8e45411
Hernan Meydac <hernan.meydac@auth0.com>
2016-05-21 19:41:19 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,76 @@
+var gynoid = require('../../index.js');
+
+module.exports = {
+  registerDroid: function(req, res) {
+    var name = req.params.name;
+    var token = req.params.token;
+
+    gynoid.registerDroid(name, token)
+      .then(function() {
+        res.text('Droid ' + name + ' successfully registered').send();
+      })
+      .catch(function(err) {
+        res.text('Unable to register Droid.\n```' + JSON.stringify(err) + '```').send();
+      });
+  },
+  unregisterDroid: function(req, res) {
+    var name = req.params.name;
+
+    gynoid.unregisterDroid(name)
+      .then(function() {
+        res.text('Droid ' + name + ' successfully unregistered').send();
+      })
+      .catch(function(err) {
+        res.text('Unable to unregister Droid.\n```' + err + '```').send();
+      });
+  },
+  reloadDroid: function(req, res) {
+    var name = req.params.name;
+
+    gynoid.reloadDroid(name)
+      .then(function() {
+        res.text('Droid ' + name + ' successfully reloaded').send();
+      })
+      .catch(function(err) {
+        res.text('Unable to reload Droid.\n```' + JSON.stringify(err) + '```').send();
+      });
+  },
+  extendDroid: function(req, res) {
+    var name = req.params.name;
+    var repo = req.params.repo;
+
+    gynoid.installFromGitHub(name, repo)
+      .then(function() {
+        res.text('Droid ' + name + ' successfully extended').send();
+      })
+      .catch(function(err) {
+        res.text('Unable to extend Droid.\n```' + JSON.stringify(err) + '```').send();
+      });
+  },
+  removeExtension: function(req, res) {
+    var name = req.params.name;
+    var extension = req.params.extension;
+
+    gynoid.removeExtension(name, extension)
+      .then(function() {
+        res.text('Extension ' + extension + ' successfully removed').send();
+      })
+      .catch(function(err) {
+        res.text('Unable to remove extension ' + extension + '.\n```' + JSON.stringify(err) + '```').send();
+      });
+  },
+  listExtensions: function(req, res) {
+    var name = req.params.name;
+    var droid = gynoid.droids[name];
+    if (!droid) {
+      return res.text('Unable to list extensions.\n```{error: "droid_unknown", message: "Could not find droid in the registry."}```').send();
+    }
+
+    var extensions = droid.extensions.map(function(extension) { return '`' + extension + '`';}).join(', ');
+    var msg = extensions.length === 0 ? 'No extensions installed' : 'Installed extensions for ' + name + ': ' + extensions;
+    return res.text(msg).send();
+  },
+  ping: function(req, res) {
+    return res.text('`Hey Pong!`').send();
+  }
+};
